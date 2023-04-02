e29b34ede697b5a6d40f68d0a2b0539698f8d909
cungen <1360571582@qq.com>
2023-04-02 16:18:13 +0800

    feat: add tailwindcss config

--- config.lua
+++ config.lua
@@ -57,6 +57,8 @@ lvim.builtin.treesitter.auto_install = true
 -- vim.list_extend(lvim.lsp.automatic_configuration.skipped_servers, { "pyright" })
 -- local opts = {} -- check the lspconfig documentation for a list of all possible options
 -- require("lvim.lsp.manager").setup("pyright", opts)
+vim.list_extend(lvim.lsp.automatic_configuration.skipped_servers, { "tailwindcss" })
+require("lvim.lsp.manager").setup("tailwindcss", {})
 
 -- ---remove a server from the skipped list, e.g. eslint, or emmet_ls. IMPORTANT: Requires `:LvimCacheReset` to take effect
 -- ---`:LvimInfo` lists which server(s) are skipped for the current filetype
